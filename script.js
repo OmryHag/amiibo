9225fe788012e344581f6b425b4eec8d6d23e673
OmryH <omry.hagag@gmail.com>
2023-04-21 17:45:26 -0600

    added initial api call

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const apiURL = 'https://www.amiiboapi.com/api/'
+const apiURL = 'https://www.amiiboapi.com/api/amiibo/'
 
 const searchBar = document.querySelector('.search-box');
 const searchInput = document.querySelector('.search-input');
 const searchButton = document.getElementById('search-button');
+const resultArea = document.querySelector('.result-area');
 
 searchButton.addEventListener('click', searchAmiibo);
 searchInput.addEventListener('keydown', (event) => {
@@ -13,5 +14,16 @@ searchInput.addEventListener('keydown', (event) => {
 })
 
 function searchAmiibo() {
-    console.log('pressed');
+    const userInput = searchInput.value.trim();
+    if (!userInput) {
+        resultArea.innerHTML = `
+            <h3>Input must not be empty!</h3>
+        `
+        return;
+    }
+
+    console.log(userInput);
+    
+    // TODO: add loader
+    fetch(apiURL + `?name=${userInput}`).then(response => response.json()).then(data => console.log(data));
 }
